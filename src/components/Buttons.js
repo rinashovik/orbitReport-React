eb116ababd7821fc7173ae306590665f48256785
Rina Shovik <rinashovik@gmail.com>
2023-08-13 12:42:20 -0500

    Testing the App

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,34 @@
-const Buttons = () => {
+import styling from './styling.css';
+import satData from './satData';
+import displaySats from '../App';
+import filterByType from '../App';
+import {useState} from 'react';
+
+
+
+    const Buttons = ({filterByType, setSat, displaySats}) => {
+ 
+
+  return(
+
+    <div className="flex-container">
+        {displaySats.map((sat, id) => {// Create only three button
+
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+    
+        <button onClick={() => filterByType(sat)} key={id}>
+        {sat} Orbit           
+        </button>      
   );
+})} 
+
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+      
+      {/* <button onChange={handleChange}> Show Me More Option</button> */}
+
+          </div>
+
+   );
 };
 
 export default Buttons;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,34 @@
-const Table = ({ sat }) => {
+
+
+import styling from './styling.css';
+import satData from './satData';
+
+const Table = ({ sat }) => { // initial state of the data 
+
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+        <th>Name</th>
+        <th>Type of Satelite</th>
+        <th>Launch date</th>
+        <th>Status</th>
         </tr>
         </thead>
-        <tbody>
-        <tr>
-          <td>Row Data TBD</td>
+        {sat.map((data, id) =>          
+
+        <tbody >
+          
+        <tr key={id}>
+          <td>{data.name}</td>
+          <td>{data.type}</td>
+          <td>{data.launchDate}</td>
+          <td>{data.operational ? "Active": "Inactive"}</td>
         </tr>
-        </tbody>
+        </tbody>)}
       </table>
   );
+
 };
 
 export default Table;
