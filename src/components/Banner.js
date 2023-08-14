b8a763da0b5fba016f32c8d022808ff1694ada6a
Rina Shovik <rinashovik@gmail.com>
2023-08-13 22:51:48 -0500

    Commiting the code

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,12 +5,10 @@ import styling from './styling.css';
 
 export default function Banner(){
 
-   return(
-    
+   return( 
     <header>
     <h1>Orbit Report</h1>
-    <p>Click on the buttons to see the satellites in that orbit type</p>
-   
+    <p>Click on the buttons to see the satellites in that orbit type</p>  
    </header>       
 
 );
